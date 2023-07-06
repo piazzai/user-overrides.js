23f2f827e3b09fb5ed200bae05e7ea2da462d4f9
piazzai <42124135+piazzai@users.noreply.github.com>
2023-07-06 19:35:31 +0200

    Revise notification preferences

--- thunderbird/user-overrides.js
+++ thunderbird/user-overrides.js
@@ -4,8 +4,9 @@ user_pref("_user.js.parrot", "OVERRIDES error: the parrot sleeps with the fishes
 user_pref("browser.safebrowsing.downloads.remote.enabled", true);                       // enable safe browsing checks for downloads
 user_pref("calendar.timezone.local", "Europe/Madrid");                                  // provide timezone to prevent automatic extraction
 user_pref("clipboard.plainTextOnly", true);                                             // do not retain formatting when copy/pasting text
-user_pref("dom.push.enabled", true)                                                     // enable push notifications
+user_pref("dom.push.enabled", false)                                                    // disable push notifications
 user_pref("dom.webnotifications.enabled", false);                                       // disable web notifications
+user_pref("dom.webnotifications.serviceworker.enabled", false);                         // disable web notification service workers
 user_pref("extensions.webextensions.restrictedDomains", "");                            // list of domains where extensions are not allowed
 user_pref("general.useragent.compatMode.firefox", true);                                // limit sending extra user-agent data
 user_pref("privacy.donottrackheader.enabled", false);                                   // disable dnt header (as it is fingerprintable)
