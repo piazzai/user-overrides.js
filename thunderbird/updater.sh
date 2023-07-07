f77657895e26365ff19231512d41d4819ea0e7a7
piazzai <42124135+piazzai@users.noreply.github.com>
2023-07-07 23:53:27 +0200

    Remove messages

--- thunderbird/updater.sh
+++ thunderbird/updater.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 if [ -f user.js ]; then
-    echo "Creating user.js backup"
     mkdir -p userjs_backups
     mv user.js "userjs_backups/user.js.backup.$(date +"%Y-%m-%d_%H%M")"
 fi
 
-echo "Downloading latest user.js"
-curl https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/master/user.js > user.js
+curl -sO https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/master/user.js
+curl -sO https://raw.githubusercontent.com/arkenfox/user.js/master/prefsCleaner.sh
 
 if [ -f user-overrides.js ]; then
-    echo "Applying user-overrides.js"
     cat user-overrides.js >> user.js
 fi
 
