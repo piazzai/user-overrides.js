f78e7d917fe0f13f73bb37dae4e610624d98817f
piazzai <42124135+piazzai@users.noreply.github.com>
2023-07-09 16:37:13 +0200

    Add echo messages

--- thunderbird/updater.sh
+++ thunderbird/updater.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ -f user.js ]; then
+    echo "Backing up current user.js..."
     mkdir -p userjs_backups
     mv user.js "userjs_backups/user.js.backup.$(date +"%Y-%m-%d_%H%M")"
 fi
@@ -9,6 +10,7 @@ curl -sO https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/mas
 curl -sO https://raw.githubusercontent.com/arkenfox/user.js/master/prefsCleaner.sh
 
 if [ -f user-overrides.js ]; then
+    echo "Applying overrides..."
     cat user-overrides.js >> user.js
 fi
 
