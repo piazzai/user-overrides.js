999f21c7c8986e63356cee5b94162bdd86b5c31b
piazzai <42124135+piazzai@users.noreply.github.com>
2023-07-09 16:36:53 +0200

    Group oauth settings

--- thunderbird/user-overrides.js
+++ thunderbird/user-overrides.js
@@ -11,17 +11,19 @@ user_pref("dom.webnotifications.enabled", false);                               
 user_pref("dom.webnotifications.serviceworker.enabled", false);                         // disable background web notifications
 user_pref("extensions.webextensions.restrictedDomains", "");                            // list of domains where extensions are not allowed
 user_pref("general.useragent.compatMode.firefox", true);                                // limit sending extra user-agent data
-user_pref("javascript.enabled", true);                                                  // enable javascript (required for Oauth2)
-user_pref("privacy.donottrackheader.enabled", false);                                   // disable dnt header (as it is fingerprintable)
+user_pref("privacy.donottrackheader.enabled", false);                                   // disable dnt header (it is fingerprintable)
 user_pref("extensions.update.autoUpdateDefault", false);                                // disable automatic installation of extension updates
 user_pref("mail.mdn.report.enabled", false);                                            // disable sending of return receipts
 user_pref("mail.wrap_long_lines", false);                                               // do not wrap lines when sending plaintext mail
 user_pref("mailnews.database.global.indexer.enabled", false);                           // disable search indexer
 user_pref("mailnews.display.prefer_plaintext", true);                                   // display messages as plaintext
 user_pref("mailnews.wraplength", 0)                                                     // disable line wrapping when sending plaintext mail
-user_pref("network.cookie.cookieBehavior", 1);                                          // accept same-site cookies (required for Oauth2)
 user_pref("offline.autoDetect", false);                                                 // disable connection status autodetection
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);                 // allow styling with userChrome.css
 user_pref("view_source.syntax_highlight", true);                                        // highlight source code of documents
 
+user_pref("network.cookie.cookieBehavior", 1);                                          // accept same-origin cookies
+user_pref("network.http.referer.XOriginPolicy", 0);                                     // always send cross-origin referrer 
+user_pref("network.http.sendRefererHeader", 2);                                         // send referrer header and set document referrer   
+user_pref("javascript.enabled", true);                                                  // enable javascript
 user_pref("_user.js.parrot", "OVERRIDES success: the parrot's alive and well.");
