d733b54538e71ae53c621e1a95eafdace20997e4
piazzai <42124135+piazzai@users.noreply.github.com>
2025-11-07 13:08:23 +0000

    Return to mbox storage

--- thunderbird/user-overrides.js
+++ thunderbird/user-overrides.js
@@ -41,7 +41,6 @@ user_pref("_user.js.parrot", "I'm a qualified brain surgeon!");
 user_pref("calendar.week.start", 1);
 user_pref("mail.accounthub.enabled", false);
 user_pref("mail.identity.default.suppress_signature_separator", true);
-user_pref("mail.serverDefaultStoreContractID", "@mozilla.org/msgstore/maildirstore;1");
 user_pref("mailnews.wraplength", 0);
 // user_pref("javascript.enabled", true);
 // user_pref("permissions.default.image", 3);
