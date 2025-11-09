a4d0f9d5a69a635e25cff47c281047caa67878c4
piazzai <42124135+piazzai@users.noreply.github.com>
2025-11-09 12:01:53 +0000

    Customize monospace font

--- thunderbird/user-overrides.js
+++ thunderbird/user-overrides.js
@@ -40,6 +40,8 @@ user_pref("_user.js.parrot", "I'm a qualified brain surgeon!");
 
 user_pref("calendar.timezone.local", "Europe/London");
 user_pref("calendar.week.start", 1);
+user_pref("font.name.monospace.x_unicode", "Iosevka Fixed");
+user_pref("font.name.monospace.x_western", "Iosevka Fixed");
 user_pref("mail.accounthub.enabled", false);
 user_pref("mail.identity.default.suppress_signature_separator", true);
 user_pref("mailnews.wraplength", 0);
